0d48422ada789584d8c3043a10814041e022c700
Mike Utley <mikey.utley@gmail.com>
2016-04-26 18:07:56 -0600

    add VCR cassettes

--- lib/smart_home/light.rb
+++ lib/smart_home/light.rb
@@ -1,14 +1,31 @@
 require 'faraday'
 require 'json'
+require 'pry'
 
 module SmartHome
 
-  API_URL = "http://localhost:3001/api/v1/lights/active"
+  API_URL = "http://localhost:3000/api/v1/lights"
 
   class Light
+    attr_reader :id, :uuid, :strength
+
+    def initialize(attributes)
+      @id = attributes["id"]
+      @uuid = attributes["uuid"]
+      @strength = attributes["strength"]
+    end
+
     def self.find(id)
       response = Faraday.get("#{API_URL}/#{id}")
       attributes = JSON.parse(response.body)
+      new(attributes)
     end
+
+    def self.all
+      response = Faraday.get("#{API_URL}")
+			lights = JSON.parse(response.body)
+			lights.map { |attributes| new(attributes) }
+    end
+
   end
 end
--- test/light/light_test.rb
+++ test/light/light_test.rb
@@ -1,13 +1,28 @@
 require './test/test_helper'
 
 class SmartHomeLightTest < Minitest::Test
+
   def test_it_returns_status_of_light
-    VCR.use_cassette('one_light') do
-      light = SmartHome::Light.find(68)
+    VCR.use_cassette('test_light') do
+      light = SmartHome::Light.find(1)
       assert_equal SmartHome::Light, light.class
 
       assert_equal 1, light.id
       assert_equal 50, light.strength
     end 
   end
+
+  def test_it_returns_all_lights
+		VCR.use_cassette('all_lights') do
+      result = SmartHome::Light.all
+
+      # Make sure we got all the lights
+      assert_equal 2, result.length
+
+      # Make sure that the JSON was parsed
+      assert result.kind_of?(Array)
+      assert result.first.kind_of?(SmartHome::Light)
+    end
+	end
+
 end
